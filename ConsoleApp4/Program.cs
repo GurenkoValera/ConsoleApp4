348122f3ae8383286a11b5b3fd970e6a10145ecd
valer <valer@DESKTOP-QD94NVR>
2024-10-06 16:42:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Инициализация переменных
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine("До обмена : a = {0}, b = {1}", a, b);
+
+        // Обмен значениями без использования дополнительных переменных
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine("После обмена : a = {0}, b = {1}", a, b);
+    }
+}
